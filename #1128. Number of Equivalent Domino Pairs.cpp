59af1c4abb1ab37d3b869cd64d317e41806f1ef5
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2025-05-04 13:59:30 +0530

    Create #1128. Number of Equivalent Domino Pairs.cpp

---
+++ #1128. Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        map<vector<int>,int>m;
+        int ans=0;
+        for(int i=0; i<dominoes.size();i++)
+        {
+            sort(dominoes[i].begin(),dominoes[i].end());
+            ans+=m[dominoes[i]];
+            m[dominoes[i]]++;
+        }
+        return ans;
+        
+        
+    }
+};
