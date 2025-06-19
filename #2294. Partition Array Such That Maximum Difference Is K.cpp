e7415bb4179dcbd7770b0b750a14ad90c2caf7ee
Anubhav Saxena <121241401+InflixOP@users.noreply.github.com>
2025-06-19 11:15:09 +0530

    Create #2294. Partition Array Such That Maximum Difference Is K.cpp

---
+++ #2294. Partition Array Such That Maximum Difference Is K.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int count=0;int mini=nums[0];
+        for(int i=1;i<nums.size();i++){
+           if(nums[i]-mini>k){
+            mini=nums[i];
+            count++;
+           }
+        }
+        return count+1;
+    }
+};
