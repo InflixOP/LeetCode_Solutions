03a77d1d97c16ae411d78ea36396097a65b8cee5
Anubhav Saxena <121241401+InflixOP@users.noreply.github.com>
2025-08-02 11:54:00 +0530

    Update #2561. Rearranging Fruits.cpp

--- #2561. Rearranging Fruits.cpp
+++ #2561. Rearranging Fruits.cpp
@@ -41,21 +41,13 @@ public:
         }
         vector<int> v1;
         vector<int> v2;
-        for(int i=0;i<n;i++){
-            if(mp1.find(basket1[i])!=mp1.end()){
-                mp1[basket1[i]]--;
-                if(mp1[basket1[i]]==0)
-                mp1.erase(basket1[i]);
-                v1.push_back(basket1[i]);
-            }
+        for(auto ele:mp1){
+            for(int i=0;i<ele.second;i++)
+            v1.push_back(ele.first);
         }
-         for(int i=0;i<n;i++){
-            if(mp2.find(basket2[i])!=mp2.end()){
-                 mp2[basket2[i]]--;
-                if(mp2[basket2[i]]==0)
-                mp2.erase(basket2[i]);
-                v2.push_back(basket2[i]);
-            }
+        for(auto ele:mp2){
+            for(int i=0;i<ele.second;i++)
+            v2.push_back(ele.first);
         }
         sort(v1.begin(),v1.end());
         sort(v2.begin(),v2.end(),greater<int>());
