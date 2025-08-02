a72feb55e3fdeef324252bd0ed5ecb52f5389050
Anubhav Saxena <121241401+InflixOP@users.noreply.github.com>
2025-08-02 11:53:13 +0530

    Update #2561. Rearranging Fruits.cpp

--- #2561. Rearranging Fruits.cpp
+++ #2561. Rearranging Fruits.cpp
@@ -39,14 +39,14 @@ public:
             ele.second=ele.second/2;;
             }
         }
-        vector<pair<int,int>> v1;
-        vector<pair<int,int>> v2;
+        vector<int> v1;
+        vector<int> v2;
         for(int i=0;i<n;i++){
             if(mp1.find(basket1[i])!=mp1.end()){
                 mp1[basket1[i]]--;
                 if(mp1[basket1[i]]==0)
                 mp1.erase(basket1[i]);
-                v1.push_back({basket1[i],i});
+                v1.push_back(basket1[i]);
             }
         }
          for(int i=0;i<n;i++){
@@ -54,18 +54,17 @@ public:
                  mp2[basket2[i]]--;
                 if(mp2[basket2[i]]==0)
                 mp2.erase(basket2[i]);
-                v2.push_back({basket2[i],i});
+                v2.push_back(basket2[i]);
             }
         }
         sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end(),greater<pair<int,int>>());
+        sort(v2.begin(),v2.end(),greater<int>());
         int min1=*min_element(basket1.begin(),basket1.end());
         int min2=*min_element(basket2.begin(),basket2.end());
         int mini=min(min1,min2);
         int i=0;
         while(i<v1.size()){
-            cout<<v1[i].first<<" "<<v2[i].first<<endl;
-            ans+=min(2*mini,min(v1[i].first,v2[i].first));
+            ans+=min(2*mini,min(v1[i],v2[i]));
             i++;
         }
         return ans;
