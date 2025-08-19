8b3e5701721c797887602bf7e3d4025ed164ffeb
Anubhav Saxena <121241401+InflixOP@users.noreply.github.com>
2025-08-19 13:59:23 +0530

    Create #2348. Number of Zero-Filled Subarrays.cpp

---
+++ #2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long c=0;
+        long long ans=0;
+        for (int i=0;i<nums.size();i++) {
+            if (nums[i]==0) {
+                c++;
+                ans+=c; 
+            }else{
+                c=0; 
+            }
+        }
+        return ans;
+    }
+};
